adbf76056c969f6ea65a77a7df695b4af2705abc
Alyssa <alyssaphelps3@gmail.com>
2026-07-05 20:59:08 +0100

    Enchanced Course page, make code more robust for guitar and amp page + Added credits to footer

--- src/elements/Footer.jsx
+++ src/elements/Footer.jsx
@@ -1,8 +1,22 @@
 function Footer(){
+    const button = {
+        width: "10%",
+        height: "10%",
+        margin: "0 5px",
+        border: 'none'
+    };
+    const icon = {
+        width: "100%",
+        height: "100%"};
+
     const CurrentYear = new Date().getFullYear();
+
     return (
         <footer className="top-bottom-elements">
-            <p>{CurrentYear} My Website</p>
+            <p>{CurrentYear} My Website</p><br/>
+            <button style={button}><a href = "https://www.linkedin.com/in/alyssa-p-2a0479358/" target="_blank"><img style = {icon} src = "https://img.icons8.com/color/1200/linkedin.jpg"/></a></button>
+            <button style={button}><a href = "https://github.com/alyssap30" target="_blank"><img style = {icon} src = "https://cdn.iconscout.com/icon/free/png-256/free-github-logo-icon-svg-download-png-8630395.png?f=webp"/></a></button>
+
         </footer>
     )
 }
--- src/pages/Amppage.jsx
+++ src/pages/Amppage.jsx
@@ -1,5 +1,5 @@
 import Header from "../elements/Header";
-import {Amps} from "../elements/Amps.jsx"; 
+import { Item } from "../elements/Item"; 
 
 export function Amppage() {
     return (
@@ -7,34 +7,47 @@ export function Amppage() {
             <Header page="Amplifiers Page" />
 
             <h2>Marshall</h2>
-            <Amps name = "Marshall MG50GFX" price = "£260" 
+            <Item name = "Marshall MG50GFX" price = "£260" type = "Amp"
             ImageSrc = "https://www.dawsons.co.uk/cdn/shop/files/products_2FM31-MG15GR_2FM31-MG15GR_1702367335830_496x496_crop_bottom.jpg?v=1702367347" 
             ImageAlt = "Marshall MG50GFX Amplifier" 
-            buyurl="https://tinyurl.com/yrecthh8" />
+            buyurl="https://tinyurl.com/yrecthh8"
+            spec1 = "50W" spec2 = "16.6kg"  spec3 = "Black/Gold" spec4 = "518 x 494 x 278 mm" spec5 = "4 Analog Channels" spec6 = "12 inch" 
+            spec7 = "Reverb, Chorus, Flanger, Phaser, Octaver, Vibrate and Delay" spec8 = "Yes" spec9 = ""/>
 
-            <Amps name = "Marshall Code 50" price = "£225"
+            <Item name = "Marshall Code 50" price = "£225" type = "Amp"
             ImageSrc = "https://thumbs.static-thomann.de/thumb/padthumb600x600/pics/bdb/_38/380977/11576144_800.jpg"
-            ImageAlt = "Marshall Code 50 Amplifier"/>
+            ImageAlt = "Marshall Code 50 Amplifier"
+            spec1 = "50W" spec2 = "13kg" spec3 = "Black" spec4 = "530 x 440 x 280 mm" spec5 = "4 Digital Channels" spec6 = "12 inch" 
+            spec7 = "24 Professional Effects including Reverb, Delay, Chorus, Flanger, Phaser, Tremolo and Vibrato" 
+            spec8 = "Yes" spec9 = "100 Presets and Bluetooth connectivity"/>
 
-            <Amps name = "Marshall JVM410H" price = "£1235"
+            <Item name = "Marshall JVM410H" price = "£1235" type = "Amp"
             ImageSrc = "https://thumbs.static-thomann.de/thumb/padthumb600x600/pics/bdb/_10/108990/6859222_800.jpg"
-            ImageAlt = "Marshall JVM310H Amplifier"/>
+            ImageAlt = "Marshall JVM310H Amplifier"
+            spec1 = "100W" spec2 = "22kg" spec3 = "Black" spec4 = "740 x 310 x 215 mm" spec5 = "4 Channels" spec6 = "N/A - Head Unit" 
+            spec7 = "Digital Reverb and Delay" spec8 = "Yes" spec9 = "Must be used with a speaker cabinet"/>
 
             <h2>Fender</h2>
-            <Amps name="Fender 65 Twin Reverb" price= "£1900"
+            <Item name="Fender 65 Twin Reverb" price= "£1900" type = "Amp"
             ImageSrc= "https://thumbs.static-thomann.de/thumb/padthumb600x600/pics/bdb/_15/153777/12531507_800.jpg"
             ImageAlt = "Fender 65 Twin Reverb Amplifier"
-            buyurl = "https://tinyurl.com/3dwnp3fx/"/>
+            buyurl = "https://tinyurl.com/3dwnp3fx/"
+            spec1 = "85W" spec2 = "29 kg" spec3 = "Black" spec4 = "673 x 267 x 5050 mm" spec5 = "2 Channels" spec6 = "2x 12 inch" 
+            spec7 = "Spring reverb and tremolo" spec8 = "Yes " spec9 = "Includes tilt-back stand"/>
 
-            <Amps name="Fender Champion II 100" price= "£310" 
+            <Item name="Fender Champion II 100" price= "£310" type = "Amp"
             ImageSrc= "https://thumbs.static-thomann.de/thumb/padthumb600x600/pics/bdb/_59/595637/19655524_800.jpg"
             ImageAlt= "Fender Champion II 100 Amplifier"
-            buyurl= "https://www.thomann.co.uk/fender_champion_ii_100.htm"/>
+            buyurl= "https://www.thomann.co.uk/fender_champion_ii_100.htm"
+            spec1 = "100W" spec2 = "18kg" spec3 = "Black" spec4 = "260 x 483 x 660 mm" spec5 = "2 Channels" spec6 = "2x 12 inch" 
+            spec7 = "Reverb, Delay, Chorus, Vibratone, Tremolo and Phaser" spec8 = "Yes" spec9 = "Built-in recorder and aux input"/>
 
-            <Amps name="Fender Mustang LT50" price= "£230"
+            <Item  name="Fender Mustang LT50" price= "£230" type = "Amp"
             ImageSrc= "https://m.media-amazon.com/images/I/71VdM7iD+uL._UF1000,1000_QL80_.jpg"
             ImageAlt= "Fender Mustang LT50 Amplifier"
-            buyurl="https://tinyurl.com/299muenz"/>
+            buyurl="https://tinyurl.com/299muenz"
+            spec1 = "50W" spec2 = "9kg" spec3 = "Black" spec4 = "432 x 216 x 419 mm" spec5 = "1 Channel" spec6 = "12 inch" 
+            spec7 = "20 Amp Models and 25 Effects" spec8 = "Yes" spec9 = "60 Memory slots for saving presets"/>
         </>
     );
 }
